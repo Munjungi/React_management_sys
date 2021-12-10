49c943845aa75b73638a6172614e2b4512bd1c02
Munjungi <jungimun@naver.com>
2021-12-11 02:28:39 +0900

    DATA WITH Database(local)

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs'); // 파일에 접근할수있는 라이브러리
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/customers', (req,res) => {
-    res.send([ 
-    {
-        'id' : 1,
-        'image' : 'https://placeimg.com/64/64/any',
-        'name' : '홍길동',
-        'birthday' : '961101',
-        'gender' : '남자',
-        'job' : '대학생'
-        },
-        {
-        'id' : 2,
-        'image' : 'https://placeimg.com/64/64/2',
-        'name' : '고길동',
-        'birthday' : '961201',
-        'gender' : '남자',
-        'job' : '직장인'
-        },
-        {
-        'id' : 3,
-        'image' : 'https://placeimg.com/64/64/3',
-        'name' : '이순신',
-        'birthday' : '221201',
-        'gender' : '남자',
-        'job' : '군인'
-        }
-    ]);
-});
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
+app.get('/api/customers', (req,res) => {
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
