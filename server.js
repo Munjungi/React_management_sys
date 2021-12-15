8e4aea2103f569d64ad8e9f7f96a1bfdc35efd0c
Munjungi <jungimun@naver.com>
2021-12-16 01:02:39 +0900

    form 데이터 전송 및 DB insert

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({dest: './upload'})
+
+// 조회
+// insert 를 위해 npm install --save multer 설치
 app.get('/api/customers', (req,res) => {
     connection.query(
         "SELECT * FROM CUSTOMER",
@@ -28,4 +33,22 @@ app.get('/api/customers', (req,res) => {
         }
     );
 });
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+    connection.query(sql, params,
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
